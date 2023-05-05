a826fd843b2694625a4538c270e9c872cfa58c31
Bijo Mathew Jose <bijomathewjose@gmail.com>
2023-05-05 16:06:36 +0530

    learning circle -api-initial complete

--- api/circle/circle_views.py
+++ api/circle/circle_views.py
@@ -1,12 +1,12 @@
 from rest_framework.views import APIView
 import requests
 from decouple import config
-from .serializers import LearningCircleSerializer
 from utils.utils_views import CustomResponse, get_current_utc_time
 from learningcircle.models import LearningCircle, CircleUserLink
 from datetime import datetime
 from uuid import uuid4
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password, check_password
+import hmac
 
 
 class CreateCircleView(APIView):
@@ -59,6 +59,7 @@ class CreateCircleView(APIView):
         # get circle code
         circle_code = self.get_circle_code(org_code, org_id, interest_group_title, interest_group_id)
         circle = LearningCircle.objects.filter(circle_code=circle_code).first()
+
         if circle:
             return CustomResponse(general_message="Circle code already exists").get_failure_response()
         # get circle name
@@ -69,7 +70,7 @@ class CreateCircleView(APIView):
             ).get_failure_response()
         circle = LearningCircle.objects.filter(name=circle_name).first()
         if circle:
-            return CustomResponse(general_message="Circle name already exists").get_failure_response()
+            return CustomResponse(general_message="Circle already exists").get_failure_response()
         # get meet place
         meet_place = request.data.get("meet_place")
         if not meet_place:
@@ -89,8 +90,9 @@ class CreateCircleView(APIView):
                 general_message="Password is required",
             ).get_failure_response()
         else:
-            password = make_password(password)
-            print(password)
+            password = hmac.new(
+                key=config("SECRET_KEY").encode(), msg=password.encode(), digestmod="SHA256"
+            ).hexdigest()
         circle = LearningCircle.objects.create(
             id=uuid4(),
             circle_code=circle_code,
@@ -119,4 +121,109 @@ class CreateCircleView(APIView):
         if user:
             return CustomResponse(general_message="Circle Created Successfully").get_success_response()
         else:
+            return CustomResponse(general_message="Failed").get_failure_response()
+
+
+class JoinCircleView(APIView):
+    def post(self, request):
+        user_id = request.headers.get("userId")
+        org_id = None
+        if user_id:
+            response = requests.get(
+                config("FR_DOMAIN_NAME") + "/api/v1/learning-circle/user-org/", headers={"userId": user_id}
+            )
+            if not response.status_code == 200:
+                return CustomResponse(general_message=response.json()["message"]).get_failure_response()
+            org_id = response.json()["response"]["org"]["id"]
+        else:
+            return CustomResponse(general_message="User id is required").get_failure_response()
+        circle_code = request.data.get("circle_code")
+        circle_id = request.data.get("circle_id")
+        if not circle_code and not circle_id:
+            return CustomResponse(general_message="Either Circle code or Circle id is required").get_failure_response()
+        circle = None
+        if circle_id:
+            circle = LearningCircle.objects.filter(id=circle_id).first()
+        elif circle_code:
+            circle = LearningCircle.objects.filter(circle_code=circle_code).first()
+        if not circle:
+            return CustomResponse(general_message="Circle not found").get_failure_response()
+        if circle.college_id != org_id:
+            return CustomResponse(general_message="User not a student of the College").get_failure_response()
+        circleUserLink = CircleUserLink.objects.filter(user_id=user_id, circle=circle).first()
+        circleUsers = CircleUserLink.objects.filter(circle=circle)
+        if circleUsers.count() >= 10:
+            return CustomResponse(general_message="Circle is full").get_failure_response()
+
+        if circleUserLink and circleUserLink.accepted:
+            return CustomResponse(general_message="Already joined").get_failure_response()
+
+        password = request.data.get("secret_key")
+        user = None
+        if password:
+            password = hmac.new(
+                key=config("SECRET_KEY").encode(), msg=password.encode(), digestmod="SHA256"
+            ).hexdigest()
+            isPasswordMatching = password == circle.password
+            if not isPasswordMatching:
+                return CustomResponse(general_message="Invalid password").get_failure_response()
+            else:
+                if circleUserLink:
+                    circleUserLink.accepted = True
+                    circleUserLink.accepted_at = get_current_utc_time()
+                    try:
+                        circleUserLink.save()
+                        return CustomResponse(general_message="Joined Successfully").get_success_response()
+                    except:
+                        return CustomResponse(general_message="Failed").get_failure_response()
+                else:
+                    user = CircleUserLink.objects.create(
+                        id=uuid4(),
+                        circle=circle,
+                        user_id=user_id,
+                        accepted=True,
+                        accepted_at=get_current_utc_time(),
+                        created_at=get_current_utc_time(),
+                    )
+                if user:
+                    return CustomResponse(general_message="Joined Successfully").get_success_response()
+                else:
+                    return CustomResponse(general_message="Failed").get_failure_response()
+        else:
+            if not circleUserLink:
+                user = CircleUserLink.objects.create(
+                    id=uuid4(),
+                    circle=circle,
+                    user_id=user_id,
+                    accepted=False,
+                    accepted_at=None,
+                    created_at=get_current_utc_time(),
+                )
+            else:
+                return CustomResponse(general_message="Already Requested").get_failure_response()
+            if user:
+                return CustomResponse(general_message="Requested Successfully").get_success_response()
+            else:
+                return CustomResponse(general_message="Failed").get_failure_response()
+
+
+class circleRequestAcceptView(APIView):
+    def post(self, request):
+        circle_id = request.headers.get("circleId")
+        user_id = request.headers.get("userId")
+        if not circle_id:
+            return CustomResponse(general_message="Circle id is required").get_failure_response()
+        if not user_id:
+            return CustomResponse(general_message="User id is required").get_failure_response()
+        circleUserLink = CircleUserLink.objects.filter(circle_id=circle_id, user_id=user_id).first()
+        if not circleUserLink:
+            return CustomResponse(general_message="User not found for circle").get_failure_response()
+        if circleUserLink.accepted:
+            return CustomResponse(general_message="Already accepted").get_failure_response()
+        circleUserLink.accepted = True
+        circleUserLink.accepted_at = get_current_utc_time()
+        try:
+            circleUserLink.save()
+            return CustomResponse(general_message="Accepted Successfully").get_success_response()
+        except:
             return CustomResponse(general_message="Failed").get_failure_response()
--- api/circle/urls.py
+++ api/circle/urls.py
@@ -1,8 +1,8 @@
 from django.urls import include, path
-from .circle_views import CreateCircleView
+from .circle_views import CreateCircleView, JoinCircleView, circleRequestAcceptView
 
 urlpatterns = [
     path("create/", CreateCircleView.as_view()),
-    # path("join/", JoinCircleView.as_view()),
-    # path("request/", RequestHandleView.as_view()),
+    path("join/", JoinCircleView.as_view()),
+    path("accept/", circleRequestAcceptView.as_view()),
 ]
